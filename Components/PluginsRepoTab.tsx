86e42b94a44a3b7e6aa0970bda9e8578f9ff5f3d
ScattrdBlade <61068313+ScattrdBlade@users.noreply.github.com>
2024-05-11 02:33:48 -0400

    Update PluginsRepoTab.tsx

--- Components/PluginsRepoTab.tsx
+++ Components/PluginsRepoTab.tsx
@@ -79,7 +79,7 @@ function ExtraPluginsTab() {
     const filteredPlugins = sortedPlugins.filter(pluginFilter);
 
     return (
-        <SettingsTab title="Plugins Repo">
+        <SettingsTab title="Plugin Repo">
             <Card className={classes("vc-settings-card", "vc-backup-restore-card")}>
                 <Flex flexDirection="column">
                     <strong>Warning</strong>
